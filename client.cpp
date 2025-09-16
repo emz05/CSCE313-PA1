1360adaed1d994c4a1730e03cecb57804f5bfca4
em <emilyjiusa@gmail.com>
2025-09-16 17:32:41 -0500

    v4

--- client.cpp
+++ client.cpp
@@ -12,6 +12,9 @@
 */
 #include "common.h"
 #include "FIFORequestChannel.h"
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fstream>
 
 using namespace std;
 
@@ -97,7 +100,8 @@ int main (int argc, char *argv[]) {
 		MESSAGE_TYPE nc = NEWCHANNEL_MSG;
     	control_chan.cwrite(&nc, sizeof(MESSAGE_TYPE));
 		// recieve name from server => var to hold name
-		// cread response from server
+		
+		// create response from server
 		// call FIFORequestChanell constructor with name from server
 		// push back new channel into vector
 	}
@@ -113,75 +117,91 @@ int main (int argc, char *argv[]) {
 		// implementation: 
 		datamsg x(p, t, e); // adjust from hardcode to user's vals
 		// send to terminal if user requests one point, else send to file
-		
+		// copy bytes at memory address of x into pre allocated space (buf) 
+			// only copy num of bytes the struct, datamsg, occupies in memory
 		memcpy(buf, &x, sizeof(datamsg));
 		chan.cwrite(buf, sizeof(datamsg)); // send request
-		double reply; // allocated space for server's response (one double: ecg value)
+		double reply; // allocated enough space for server's response as doubles take up 8 bytes (one double: ecg value)
+		// place response into address of reply (pointing to location to store)
 		chan.cread(&reply, sizeof(double)); //answer, response
 		cout << "For person " << p << ", at time " << t << ", the value of ecg " << e << " is " << reply << endl;
 	}
 	// problem: when user provides only p statement, get first 1000 data points of ecg1 & ecg2 and save to file named x1.csv
 	// solution: 
+		// 1000* { 2*{ allocate space for request
+		// store request into var
+		// copy raw bytes of request from var's address into allocated space
+		// write copy of request to server with as much bytes datamsg obj takes
+		// allocate space for response
+		// read response and transfer into address of allocated space 
+		// send response to file name x1.csv in recieved folder }}
+
 	// implementation:
 	// 1000 data point retrival
-	else if(p != -1){
+	else if(p != -1 && t == -1 && e == -1){
+		std::ofstream outfile("/recieved/x1.csv");
 		for(int i =0; i < 1000; ++i){
 			char buf[MAX_MESSAGE]; 
 			// time moves in .004 increments
-			t= i;
-			datamsg x(p, t, 1); 
-			memcpy(buf, &x, sizeof(datamsg));
-			chan.cwrite(buf, sizeof(datamsg)); 
-			double ecg1, ecg2; 
-			chan.cread(&ecg1, sizeof(double)); 
-			datamsg x(p, t[i], 2); 
-			memcpy(buf, &x, sizeof(datamsg));
-			chan.cwrite(buf, sizeof(datamsg)); 
-			// send reply to file named x1.csv in recieved folder
-
-			t = (1+(.004*i)) - i;
-			// i = 1: 1.004 - 1 = .004
-			// i = 2: 1.008 - 1 = .008
-			// i = 3: 1.012 - 1 = .012
+			t=i*.004;
+			for(int j =1; j < 3; ++j){
+				datamsg x(p, t, j); 
+				memcpy(buf, &x, sizeof(datamsg));
+				chan.cwrite(buf, sizeof(datamsg)); 
+				double reply;
+				chan.cread(&reply, sizeof(double)); 
+				if(j == 1){
+					outfile << reply << ", ";
+				}
+				else{
+					outfile << reply << endl;
+				}
+			}
 		}
+		outfile.close();
 	}
 	
-	
-    // sending a non-sense message, you need to change this
-	filemsg fm(0, 0);
-	string fname = filename;
+	else if(filename != ""){
+		// sending a non-sense message, you need to change this
+		filemsg fm(0, 0);
+		string fname = filename;
 
-	// problem: when user provides -f filename, transfer entire file in sections (files are too big to send all at once)
-		// (offset: starting pt, length)
-		// account for last transfer where byte size might be different than default MAX_MESSAGE buffer capacity
-	// solution:
-	// implementation:
-	
-	// +1 accounts for null terminator 
-	int len = sizeof(filemsg) + (fname.size() + 1);
-	char* buf2 = new char[len];
-	memcpy(buf2, &fm, sizeof(filemsg)); // copy file message into buffer
-	strcpy(buf2 + sizeof(filemsg), fname.c_str()); // copy file name into buffer
-	chan.cwrite(buf2, len);  // I want the file length; <- send buffer to server (file request)
-	// file request returns file length
-	int64_t filesize = 0; // stores how many bytes are in the file
-	chan.cread(&filesize, sizeof(int64_t));
-
-	// size of max server response
-	char* buf3 = new char[m];
-
-	for(int i =0; i <= filesize/m; ++i){
-		int length = 0;
-		filemsg* file_req = (filemsg*) buf2;
-		file_req->offset = i + length; 
-		// problem: cannot request more bytes than needed or else segmentation in server
-		// solution: length: min(buffer capacity, total file bytes % buffer capacity: remainder)
+		// problem: when user provides -f filename, transfer entire file into recieved directory (send in sections as files are too big to send all at once)
+			// (offset: starting pt, length)
+			// account for last transfer where byte size might be different than default buffer capacity
+		// solution:
 		// implementation:
-		file_req->length = min(m, filesize%m);
-		chan.cwrite(buf2, len);
-		chan.cread(&buf3, file_req->length);
-		// write buf3 into file in recieved directory
+		
+		// +1 accounts for null terminator 
+		int len = sizeof(filemsg) + (fname.size() + 1);
+		char* buf2 = new char[len];
+		memcpy(buf2, &fm, sizeof(filemsg)); // copy file message into buffer
+		strcpy(buf2 + sizeof(filemsg), fname.c_str()); // copy file name into buffer
+		chan.cwrite(buf2, len);  // I want the file length; <- send buffer to server (file request)
+		// file request returns file length
+		int64_t filesize = 0; // stores how many bytes are in the file
+		chan.cread(&filesize, sizeof(int64_t));
 
+		// size of max server response
+		char* buf3 = new char[m];
+
+		for(int i =0; i <= filesize/m; ++i){
+			int length = 0;
+			filemsg* file_req = (filemsg*) buf3;
+			file_req->offset = i + length; 
+			// problem: cannot request more bytes than needed or else segmentation in server
+			// solution: length: min(buffer capacity, total file bytes % buffer capacity: remainder)
+			// implementation:
+			file_req->length = min(m, filesize%m);
+			chan.cwrite(buf2, len);
+			chan.cread(&buf3, file_req->length);
+			// write buf3 into file in recieved directory
+
+		}
+	}
+
+	else{
+		// unknown message
 	}
 
 	delete[] buf2;
@@ -194,6 +214,10 @@ int main (int argc, char *argv[]) {
 	// closing the channel    
     MESSAGE_TYPE m = QUIT_MSG;
     chan.cwrite(&m, sizeof(MESSAGE_TYPE));
+
+	// parent cleans up after child
+	int status;
+	waitpid(pid, &status, 0);
 }
 
 	// notes:
